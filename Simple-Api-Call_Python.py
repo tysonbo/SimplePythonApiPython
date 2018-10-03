93b1d121eefbeb6c0ba61d2da9be1e4012f7b486
Ty Bodager <tybodager@yahoo.com>
2018-10-03 15:34:24 -0400

    initial commit

---
+++ Simple-Api-Call_Python.py
@@ -0,0 +1,20 @@
+import urllib.parse
+import requests
+
+
+api_url = 'http://maps.googleapis.com/maps/api/geocode/json?'
+
+while True:
+	address = input('Address: ')
+	url = api_url + urllib.parse.urlencode({'address': address})
+	print(url)
+	json_response = requests.get(url).json()
+
+	json_status = json_response['status']
+	print('API Status: ' + json_status)
+	print();
+
+	if json_status == 'OK':
+		for each in json_response['results'][0]['address_components']:
+
+			print(each['long_name'])
